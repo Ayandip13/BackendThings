d0130b20aa0b6e96b5f511d6973316f4d576ee4e
Ayandip paul <152761756+Ayandip13@users.noreply.github.com>
2025-06-29 15:48:15 +0530

    add initializations

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = 8000;
-const path = require("path");
-
-const staticPath = path.join(__dirname, "public");
-app.use(express.static(staticPath));
-
-app.listen(PORT, () => console.log("server running at", PORT));
