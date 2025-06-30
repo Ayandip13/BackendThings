bf81f99df16f1e21b02e891ce372e0f6e27c0033
Ayandip paul <152761756+Ayandip13@users.noreply.github.com>
2025-06-30 07:53:42 +0530

    feat: implement book creation with Cloudinary image upload and authentication middleware

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -1,9 +1,35 @@
 import express from "express";
+import cloudinary from "../lib/cloudinary";
+import Book from "../models/Book";
+import protectRoute from "../middleware/auth.middleware";
 
 const router = express.Router();
 
+router.post("/", protectRoute, async (req, res) => {
+  try {
+    const { title, caption, image, rating } = req.body;
+    if (!image || !title || !caption || !rating)
+      return res.status(400).json({ message: "Please provide all fields" });
 
+    //upload the image to cloudinary
+    const uploadResponse = await cloudinary.uploader.upload(image);
+    const imageUrl = uploadResponse.secure_url;
 
+    //save these into the database
+    const newBook = new Book({
+      title,
+      caption,
+      image: imageUrl,
+      rating,
+      user: req.user._id,
+    });
 
+    await newBook.save();
+    res.status(201).json(newBook);
+  } catch (error) {
+    console.log("Error creating the book", error);
+    res.status(500).json({ message: error.message });
+  }
+});
 
 export default router;
