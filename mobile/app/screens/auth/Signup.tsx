70d1ec328f019b144e7f5f87da19984146955f27
Ayandip paul <152761756+Ayandip13@users.noreply.github.com>
2025-07-13 23:47:44 +0530

    feat: implement Signup screen with styled components and input fields for username, email, and password

--- mobile/app/screens/auth/Signup.tsx
+++ mobile/app/screens/auth/Signup.tsx
@@ -1,31 +1,149 @@
-import { View, Text, TouchableOpacity } from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  Platform,
+  KeyboardAvoidingView,
+  TextInput,
+  // ActivityIndicator,
+  TouchableOpacity,
+  // Alert,
+} from 'react-native';
+import React, { useState } from 'react';
+import styles from '../../../assets/styles/signup.styles.js';
+import Ionicons from "@expo/vector-icons/Ionicons";
+// TODO: need to implement for cli setup
+import COLORS from '../../constants/COLOR.js';
 import { useNavigation } from '@react-navigation/native';
-import Signin from './Signin';
+// import { router, useRouter } from "expo-router";
 
-const Signup = () => {
+export default function Signup() {
+  const [username, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
   const navigation = useNavigation();
+  // const { user, isLoading, register, token } = useAuthStore();
+
+  // const router = useRouter();
+
+  // const handleSignup = async () => {
+  //   // setIsLoading(true);
+  //   const result = await register(username, email, password);
+
+  //   if (!result.success) Alert.alert("Error", result.error);
+  // };
+
+  // console.log(user);
+  // console.log(token);
 
   return (
-    <View>
-      <Text style={{ textAlign: 'center' }}>Signup</Text>
-      <TouchableOpacity
-        onPress={() => navigation.navigate('Signin')}
-        style={{
-          marginTop: 100,
-          backgroundColor: 'blue',
-          alignItems: 'center',
-          // padding:10,
-          justifyContent: 'center',
-          paddingHorizontal: '3%',
-          paddingVertical: '3%',
-          borderRadius: 50,
-        }}
-      >
-        <Text style={{ color: '#fff' }}>Click to go onto the next screen</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <View style={styles.container}>
+        <View style={styles.card}>
+          <View style={styles.header}>
+            <Text style={styles.title}>Bookosaurs 🐲</Text>
+            <Text style={styles.subtitle}>Share Your favourite reads</Text>
+          </View>
+          <View style={styles.formContainer}>
+            {/* Username input */}
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Username</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                  name="person-outline"
+                  size={20}
+                  color={COLORS.primary}
+                  style={styles.inputIcon}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="eg. bruise w.."
+                  autoCapitalize="none"
+                  onChangeText={setUserName}
+                  value={username}
+                  placeholderTextColor={COLORS.placeholderText}
+                />
+              </View>
+            </View>
+            {/* email input */}
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Email</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                  name="mail-outline"
+                  size={20}
+                  color={COLORS.primary}
+                  style={styles.inputIcon}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="bwayne99@.."
+                  autoCapitalize="none"
+                  onChangeText={setEmail}
+                  value={email}
+                  placeholderTextColor={COLORS.placeholderText}
+                />
+              </View>
+            </View>
+            {/* password input */}
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Password</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                  onPress={() => setShowPassword(!showPassword)}
+                  name={showPassword ? "eye" : "eye-off"}
+                  size={20}
+                  color={COLORS.primary}
+                  style={styles.inputIcon}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Enter your password"
+                  autoCapitalize="none"
+                  placeholderTextColor={COLORS.placeholderText}
+                  value={password}
+                  onChangeText={setPassword}
+                  secureTextEntry={!showPassword}
+                />
+              </View>
+            </View>
+            {/* 
+            <TouchableOpacity
+              style={styles.button}
+              activeOpacity={0.7}
+              // onPress={handleSignup}
+              // disabled={isLoading}
+            >
+              {isLoading ? (
+                <ActivityIndicator size="small" color="#fff" />
+              ) : (
+                <Text style={styles.buttonText}>Sign Up</Text>
+              )}
+            </TouchableOpacity> */}
 
-export default Signup;
+            <TouchableOpacity
+              style={styles.button}
+              activeOpacity={0.7}
+              // onPress={handleSignup}
+              // disabled={isLoading}
+            >
+                <Text style={styles.buttonText}>Sign Up</Text>
+            </TouchableOpacity>
+
+            {/* footer */}
+
+            <View style={styles.footer}>
+              <Text>Already have an account </Text>
+              <TouchableOpacity onPress={() => navigation.goBack()}>
+                <Text style={styles.link}>Login</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
