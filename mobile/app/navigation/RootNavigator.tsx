bd119ea541b25309f88c21173dec79c6cb929fc0
Ayandip paul <152761756+Ayandip13@users.noreply.github.com>
2025-07-26 23:40:45 +0530

    delete all mobile things

--- mobile/app/navigation/RootNavigator.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-import AuthStack from './AuthStack';
-import BottomTabs from './BootomTab';
-
-export default function RootNavigator() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // later replace with context/auth state
-
-  return isLoggedIn ? <BottomTabs /> : <AuthStack />;
-}
